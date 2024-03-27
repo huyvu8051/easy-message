910fba830c9291df5228062319a62895438d579b
huyvu8051 <huyvu8051@gmail.com>
2024-03-27 20:38:18 +0700

    move api to app route

--- pages/api/socket/messages.ts
+++
@@ -1,6 +0,0 @@
-import {NextApiRequest} from "next";
-import {NextApiResponseServerIo} from "@/types";
-
-export default async function handler(req: NextApiRequest, res: NextApiResponseServerIo) {
-    res.socket.server.io.emit('notification', {message: 'dick'})
-}
